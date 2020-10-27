3ca6550d00d4fe7943cd5421ec6d78a1ae362071
Sofyan Ardianto <sofyanard@gmail.com>
2020-10-27 16:08:42 +0700

    fix error repository saat pindah ke database postgresql

--- src/main/java/com/mert/repository/AppGroupMenuRepository.java
+++ src/main/java/com/mert/repository/AppGroupMenuRepository.java
@@ -10,7 +10,7 @@ import com.mert.model.AppGroupMenu;
 
 public interface AppGroupMenuRepository extends JpaRepository<AppGroupMenu, Integer> {
 	
-	@Query(value = "select m.* from appgroupmenu m where m.group_id = :groupId order by m.menu_id", nativeQuery=true)
+	@Query(value = "select m.* from appgroupmenu m where m.group_id = cast(:groupId as varchar) order by m.menu_id", nativeQuery=true)
 	List<AppGroupMenu> findByGroup(@Param("groupId") String groupId);
 	
 	@Query(value = "select g.* from appgroupmenu g join appmenu m on g.menu_id = m.menu_id where m.level = 1 and g.group_id = :groupId order by g.menu_id", nativeQuery=true)
