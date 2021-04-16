329e6f1a0d296eca5488536ca8ef975a97693dde
Sofyan Ardianto <sofyanard@gmail.com>
2021-04-16 14:36:05 +0700

    Bug Fix

--- src/main/java/com/mert/model/NasabahLapKeu.java
+++ src/main/java/com/mert/model/NasabahLapKeu.java
@@ -16,7 +16,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class NasabahLapKeu {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Integer id;
 	
